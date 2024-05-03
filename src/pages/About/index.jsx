4a4f564a3c42dd2e32c0735b9a06dab04160aea2
Yves G <gouchereyves@gmail.com>
2024-05-03 19:53:59 +0200

    poc

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,4 @@
+const About = () => {
+  return <>Welcome to about page</>
+}
+export default About
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <>
+      <p>Welcome to home page</p>
+    </>
+  )
+}
+export default Home
